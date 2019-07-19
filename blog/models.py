a5c7e7e362404cafb0cbb2f8d7facf18b89de9d9
sedherthe <siddhartha.soma1@gmail.com>
2019-07-19 07:58:33 +0530

    Update the Post attribute field name to 'date-published'

--- blog/models.py
+++ blog/models.py
@@ -8,5 +8,5 @@ from django.contrib.auth.models import User
 class Post(models.Model):
 	title = models.CharField(max_length=100)
 	content = models.TextField()
-	data_published = models.DateTimeField(default=timezone.now)
+	date_published = models.DateTimeField(default=timezone.now)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
