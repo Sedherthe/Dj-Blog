80f11c19c41ddc0605197d4baba23499381d5587
sedherthe <siddhartha.soma1@gmail.com>
2019-07-20 15:49:09 +0530

    Change crispy template pack to bootstrap4

--- django_project/settings.py
+++ django_project/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# crispy forms by default user Bootstrap 2. Change to v4
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
