fc52d932db797cbda71ef23070d35f8e2fce7110
sedherthe <siddhartha.soma1@gmail.com>
2019-07-21 23:56:59 +0530

    Update the views and save method of the Profile model for image field

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 """
 	Options with messages:
@@ -35,4 +35,27 @@ def register(request):
 # Using a decorator which makes login mandatory for this view.(profile)
 @login_required
 def profile(request):
-	return render(request, 'users/profile.html')
+
+	if request.method == 'POST':
+		u_form = UserUpdateForm(request.POST, instance=request.user)
+		p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+		
+		if u_form.is_valid() and p_form.is_valid():
+			u_form.save()
+			print(request.user.profile.image.path)
+			p_form.save()
+			print(request.user.profile.image.path)
+			print(f'Profile Successfully Updated!')
+			messages.success(request, f'Profile Updated Successfully!')
+			# Post Get redirect pattern problem. are you sure you wanna reload problem.
+			return redirect('user-profile')
+	else:
+		u_form = UserUpdateForm(instance=request.user)
+		p_form = ProfileUpdateForm(instance=request.user.profile)
+
+	context = {
+		'u_form' : u_form, 
+		'p_form' : p_form
+	}
+
+	return render(request, 'users/profile.html', context)
