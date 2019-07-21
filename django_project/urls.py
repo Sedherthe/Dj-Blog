2fb9043459e0036558e97f2b360d60db7ac9600f
sedherthe <siddhartha.soma1@gmail.com>
2019-07-21 16:56:27 +0530

    Update the url patterns to show static media files for development only

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as user_views
 from django.contrib.auth import views as auth_views
 
@@ -26,3 +28,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='user-login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='user-logout'),
 ]
+
+# We should deploy in this way only when we are deveplopment mode
+# Not for production mode.
+if settings.DEBUG:
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
